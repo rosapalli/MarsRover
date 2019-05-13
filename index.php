23ed19a3b60c628425cdfe5a9169a9e21438570b
Pal <Pal@LAPTOP-EI331EBV.mynet>
2019-05-13 01:02:31 +0100

    almost fully functional version - need to review and update Factory to ensure all movements are correct

--- index.php
+++ index.php
@@ -7,12 +7,14 @@ spl_autoload_register(function ($class) {
 $file = file('input.txt');
 $factory = New Factory();
 
+//ABILITY TO WRITE INTO THE FILE HERE
+
 foreach ($file as $input) {
     if (preg_match("/[0-9]\s[0-9]\s[A-Z]/", $input)) {
-        if (isset($rover)) {
-            $rover2 = $factory->createRover($input);
-        } else if (isset($rover2)) {
+        if (isset($rover2)) {
             $rover3 = $factory->createRover($input);
+        } else if (isset($rover)) {
+            $rover2 = $factory->createRover($input);
         } else {
             $rover = $factory->createRover($input);
         }
@@ -22,10 +24,10 @@ foreach ($file as $input) {
         }
         $plateau = $factory->createPlateau($input);
     } else {
-        if (isset($commands)) {
-            $commands2 = $factory->createCommands($input);
-        } else if (isset($rover2)) {
+        if (isset($commands2)) {
             $commands3 = $factory->createCommands($input);
+        } else if (isset($commands)) {
+            $commands2 = $factory->createCommands($input);
         } else {
             $commands = $factory->createCommands($input);
         }
@@ -34,5 +36,7 @@ foreach ($file as $input) {
 
 $factory->createMovement($rover, $plateau, $commands);
 $factory->createMovement($rover2, $plateau, $commands2);
+$factory->createMovement($rover3, $plateau,$commands3);
 
-echo $rover->getRover() . PHP_EOL . $rover2->getRover();
+//CREATE NEW FILE FOR OUTPUT AND WRITE THE FOLLOWING
+echo $rover->getRover() . PHP_EOL . $rover2->getRover() . PHP_EOL . $rover3->getRover();
